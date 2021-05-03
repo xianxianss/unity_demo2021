1337817c8b12941132016e9deec6e13b2d43393a
xianxianss <75553503+xianxianss@users.noreply.github.com>
2021-05-04 00:09:43 +0800

    5/4

---
+++ demo2021/Assets/Scripts/ShowAnime.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using Unity.RemoteConfig;
+
+public class ShowAnime : MonoBehaviour
+{
+
+    public struct userAttributes { }
+    public struct appAttributes { }
+
+    public bool Q1A1 = false;
+
+    public GameObject btn0l;
+    public GameObject btn02;
+
+    //static int counts = 0;
+
+
+
+    /*ublic void restart()
+    {
+        btnl2.SetActive(true);
+    }*/
+
+
+    public void isonClick()
+    {
+        btn0l.SetActive(false);
+        ConfigManager.FetchCompleted += Q1configs;
+        ConfigManager.FetchConfigs<userAttributes, appAttributes>(new userAttributes(), new appAttributes());
+    }
+
+
+
+
+    public void Q1configs(ConfigResponse response)
+    {
+        Q1A1 = ConfigManager.appConfig.GetBool("Q1A1");
+
+        if (Q1A1)
+        {
+            //GetComponent<ShowBtn>().btn1.SetActive(true);
+            GetComponent<button>().onClick();
+
+        }
+
+    }
+
+
+    // public void Vote()
+    //{
+    // counts++;
+    // Q1A1 = ConfigManager.appConfig.GetInt("Q1A1",counts);
+    //  if (Q1A1)
+    //{
+    // counts[0]++;
+    //}
+
+    //}
+
+}
+
---
+++ demo2021/Assets/Scripts/ShowBtn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class ShowBtn : MonoBehaviour
+{
+    private VideoClip vc1;
+    private VideoPlayer v;
+    public GameObject btn1;
+   
+
+
+    private void Awake()
+    {
+        v = GetComponent<VideoPlayer>();
+        vc1 = v.clip;
+        Invoke("Show", (float)vc1.length);
+     
+
+    }
+
+    private void Show()
+    {
+        btn1.SetActive(true);
+       
+    }
+    
+}
